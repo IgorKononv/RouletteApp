c5c24ddfe677fae61a86bcf8ae367280dcce2745
Qwertu-Hacker <109757033+Qwertu-Hacker@users.noreply.github.com>
2023-08-17 13:43:49 +0300

    create project

---
+++ RouletteApp/RouletteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteAppApp.swift
+//  RouletteApp
+//
+//  Created by Igor Kononov on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RouletteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
